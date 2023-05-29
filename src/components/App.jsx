60d344a6e4bf6d9549f1fbe71f7274a139b643ca
Heorhii Honcharov <0019@ukr.net>
2023-05-29 22:10:26 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+
+import { Profile } from 'components/profile/Profile';
+import { Statistics } from 'components/statistics/Statistics';
+import { FriendList } from 'components/friendList/FriendList';
+import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <FriendList friends={friends} />
+      {/* with Header */}
+      <Statistics title="Upload stats" stats={data} />
+      {/* without Header */}
+      <Statistics stats={data} />
+      <TransactionHistory items={transactions} />
+    </section>
   );
 };
